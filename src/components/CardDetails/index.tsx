300f4d10a473199cc1cd1a737d2beef67b501117
akthakur4744 <37959900+akthakur4744@users.noreply.github.com>
2023-12-17 09:33:59 +0530

    Update index.tsx

--- src/components/CardDetails/index.tsx
+++ src/components/CardDetails/index.tsx
@@ -12,44 +12,64 @@ import CardDetailComponent from "./CardDetailComponent";
 import { BankCardPropsType } from "../BankCard";
 
 interface CardDetailsSectionProps {
-  activeCard: BankCardPropsType
+  activeCard: BankCardPropsType;
   children?: React.ReactNode;
 }
-export default function CardDetailsSection(CardDetailsSectionProps: CardDetailsSectionProps) {
+export default function CardDetailsSection(
+  CardDetailsSectionProps: CardDetailsSectionProps
+) {
   const { activeCard } = CardDetailsSectionProps;
   return (
     <Container sx={{ background: "#FFFFFF", padding: "24px" }}>
-      <AccordionGroup sx={{ maxWidth: 400, marginBottom: 8, gap: "24px"}}>
-      <Accordion sx={{  border: "1px solid #F0F0F0", borderRadius: "10px" }} defaultExpanded={true}>
-        <AccordionSummary
-          indicator={<img src={DownArrow} alt="down_arrow" />}
-          sx={{ background: "#FAFCFF", height: "72px", borderRadius: "10px 10px 0 0" }}
+      <AccordionGroup sx={{ maxWidth: 400, marginBottom: 8, gap: "24px" }}>
+        <Accordion
+          sx={{ border: "1px solid #F0F0F0", borderRadius: "10px" }}
+          defaultExpanded={true}
         >
-          <img src={CardSectionLogo} alt="trans_logo" /> Card Details
-        </AccordionSummary>
-        <AccordionDetails sx={{ border: "#F5F5F5" }}>
-          {
-            activeCard && (Object.entries(activeCard).map(([key, value]) => {
-              return (
-                <CardDetailComponent attributeName={key} attributeValue={value} />
-              )
-            } ))
-          }
-        </AccordionDetails>
-      </Accordion>
-      
-      <Accordion sx={{ border: "1px solid #F5F5F5",  borderRadius: "10px" }} defaultExpanded={true}>
-        <AccordionSummary
-          indicator={<img src={DownArrow} alt="down_arrow" />}
-          sx={{ background: "#FAFCFF", height: "72px", borderRadius: "10px 10px 0 0" }}
+          <AccordionSummary
+            indicator={<img src={DownArrow} alt="down_arrow" />}
+            sx={{
+              background: "#FAFCFF",
+              height: "72px",
+              borderRadius: "10px 10px 0 0",
+            }}
+          >
+            <img src={CardSectionLogo} alt="trans_logo" /> Card Details
+          </AccordionSummary>
+          <AccordionDetails sx={{ border: "#F5F5F5" }}>
+            {activeCard &&
+              Object.entries(activeCard).map(([key, value]) => {
+                if (key === "isFrozen") return null;
+                return (
+                  <CardDetailComponent
+                    key={key}
+                    attributeName={key}
+                    attributeValue={value}
+                  />
+                );
+              })}
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion
+          sx={{ border: "1px solid #F5F5F5", borderRadius: "10px" }}
+          defaultExpanded={true}
         >
-          <img src={TransactionLogo} alt="trans_logo" /> Recent Transactions
-        </AccordionSummary>
-        <AccordionDetails sx={{ border: "#F5F5F5" }}>
-          <RecentTransactions />
-        </AccordionDetails>
-      </Accordion>
-    </AccordionGroup>
+          <AccordionSummary
+            indicator={<img src={DownArrow} alt="down_arrow" />}
+            sx={{
+              background: "#FAFCFF",
+              height: "72px",
+              borderRadius: "10px 10px 0 0",
+            }}
+          >
+            <img src={TransactionLogo} alt="trans_logo" /> Recent Transactions
+          </AccordionSummary>
+          <AccordionDetails sx={{ border: "#F5F5F5" }}>
+            <RecentTransactions />
+          </AccordionDetails>
+        </Accordion>
+      </AccordionGroup>
     </Container>
   );
 }
